0cf703ad87b3a051b675c7cf8b86376d0809a0d8
235005 <235005@KB37-303-C5>
2025-10-15 15:24:48 +0300

    Добавьте файлы проекта.

---
+++ lesson5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversityManagementSystem
+{
+    // Перечисление для академических степеней преподавателей
+    public enum AcademicDegree
+    {
+        Bachelor,
+        Master,
+        Doctor,
+        Professor
+    }
+}
